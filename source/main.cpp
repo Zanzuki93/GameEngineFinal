08d09b6c97ce064db1d70badb5685d12e875e700
Zanzuki93 <chazbonner@yahoo.com>
2016-07-30 22:28:31 -0500

    Done With The GUI :D

--- source/main.cpp
+++ source/main.cpp
@@ -25,7 +25,6 @@
 
 using namespace std;
 
-
 #include <stdio.h>
 #include <iostream>
 int main(int argc, char* argv[])
@@ -51,7 +50,9 @@ string currentWorkingDirectory(getcwd(NULL, 0));
 #endif
 
  SDL_Renderer* r1;
-
+ int playerHealth = 100;
+ int ammoCount = 10;
+ bool hasBlackKey = false, hasPurpleKey = false, hasPinkKey = false;
  bool inGame = true;
  SDL_Event event;
 
@@ -96,67 +97,67 @@ Wall.x = 1220;
 Wall.y = -620;
 Wall.w = 30;
 Wall.h = 1300;
-//SDL_Texture * w1 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Texture * w1 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
 //Left Wall
 SDL_Rect Wall2;
 Wall2.x = 0;
 Wall2.y = -1270;
 Wall2.w = 10;
 Wall2.h = 1970;
-//SDL_Texture * w2 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Texture * w2 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
 //Roof
 SDL_Rect Wall3;
 Wall3.x = 0;
 Wall3.y = -1270;
 Wall3.w = 1650;
 Wall3.h = 30;
-//SDL_Texture * w3 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
+SDL_Texture * w3 = IMG_LoadTexture(r1, (images_dir+"WallTexturePlaceholder.png").c_str());
 //Floor
 SDL_Rect Wall4;
 Wall4.x = 0;
 Wall4.y = 670;
 Wall4.w = 1650;
 Wall4.h = 30;
-//SDL_Texture * w4 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w4 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 //Right Wall
 SDL_Rect Wall5;
 Wall5.x = 460;
 Wall5.y = -790;
 Wall5.w = 30;
 Wall5.h = 1475;
-//SDL_Texture * w5 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w5 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 //Right Wall
 SDL_Rect Wall6;
 Wall6.x = 1650;
 Wall6.y = -1270;
 Wall6.w = 10;
 Wall6.h = 1970;
-//SDL_Texture * w6 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w6 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 //Ceiling For Rooms
 SDL_Rect Wall7;
 Wall7.x = 0;
 Wall7.y = -790;
 Wall7.w = 40;
 Wall7.h = 30;
-//SDL_Texture * w7 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w7 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 SDL_Rect Wall8;
 Wall8.x = 150;
 Wall8.y = -790;
 Wall8.w = 325;
 Wall8.h = 20;
-//SDL_Texture * w8 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w8 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 SDL_Rect Wall9;
 Wall9.x = 650;
 Wall9.y = -790;
 Wall9.w = 1000;
 Wall9.h = 20;
-//SDL_Texture * w9 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w9 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 SDL_Rect Wall10;
 Wall10.x = 990;
 Wall10.y = -620;
 Wall10.w = 500;
 Wall10.h = 20;
-//SDL_Texture * w10 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
+SDL_Texture * w10 = IMG_LoadTexture(r1, (images_dir + "WallTexturePlaceholder.png").c_str());
 
 //Creating a pickup
 SDL_Rect healthPickUp;
@@ -172,6 +173,77 @@ ammoPickUp.y = 620;
 ammoPickUp.w = 30;
 ammoPickUp.h = 30;
 SDL_Texture * AmmoPkUp1 = IMG_LoadTexture(r1,(images_dir+"ammoPickUp.png").c_str());
+//Creating the Health Bar
+SDL_Rect HealthBarFront;
+HealthBarFront.x = 100;
+HealthBarFront.y = 50;
+HealthBarFront.w = 200;
+HealthBarFront.h = 30;
+SDL_Texture * HBarFront = IMG_LoadTexture(r1, (images_dir + "healthBar.png").c_str());
+SDL_Rect HealthBarBack;
+HealthBarBack.x = 100;
+HealthBarBack.y = 50;
+HealthBarBack.w = 200;
+HealthBarBack.h = 30;
+SDL_Texture * HBarBack = IMG_LoadTexture(r1, (images_dir + "healthBarBackground.png").c_str());
+//Creating the Mana Bar
+SDL_Rect ManaBarFront;
+ManaBarFront.x = 100;
+ManaBarFront.y = 125;
+ManaBarFront.w = 200;
+ManaBarFront.h = 30;
+SDL_Texture * MBarFront = IMG_LoadTexture(r1, (images_dir + "ammoGUI.png").c_str());
+SDL_Rect ManaBarBack;
+ManaBarBack.x = 100;
+ManaBarBack.y = 125;
+ManaBarBack.w = 200;
+ManaBarBack.h = 30;
+SDL_Texture * MBarBack = IMG_LoadTexture(r1, (images_dir + "ammoGUIBackground.png").c_str());
+//Creating the GUI For the Keys
+SDL_Rect PinkKeyGUI;
+PinkKeyGUI.x = 100;
+PinkKeyGUI.y = 200;
+PinkKeyGUI.w = 50;
+PinkKeyGUI.h = 50;
+SDL_Texture * PinkKeyGUITexture = IMG_LoadTexture(r1, (images_dir + "pinkKeyPickUp.png").c_str());
+SDL_Rect PurpleKeyGUI;
+PurpleKeyGUI.x = 150;
+PurpleKeyGUI.y = 200;
+PurpleKeyGUI.w = 50;
+PurpleKeyGUI.h = 50;
+SDL_Texture * PurpleKeyGUITexture = IMG_LoadTexture(r1, (images_dir + "purpleKeyPickUp.png").c_str());
+SDL_Rect BlackKeyGUI;
+BlackKeyGUI.x = 200;
+BlackKeyGUI.y = 200;
+BlackKeyGUI.w = 50;
+BlackKeyGUI.h = 50;
+SDL_Texture * BlackKeyGUITexture = IMG_LoadTexture(r1, (images_dir + "blackKeyPickUp.png").c_str());
+//Creating the Keys 
+SDL_Rect PinkKey;
+PinkKey.x = 500;
+PinkKey.y = 550;
+PinkKey.w = 30;
+PinkKey.h = 30;
+SDL_Texture * PinkKeyTexture = IMG_LoadTexture(r1, (images_dir + "pinkKeyPickUp.png").c_str());
+SDL_Rect PurpleKey;
+PurpleKey.x = 700;
+PurpleKey.y = 550;
+PurpleKey.w = 30;
+PurpleKey.h = 30;
+SDL_Texture * PurpleKeyTexture = IMG_LoadTexture(r1, (images_dir + "purpleKeyPickUp.png").c_str());
+SDL_Rect BlackKey;
+BlackKey.x = 600;
+BlackKey.y = 550;
+BlackKey.w = 30;
+BlackKey.h = 30;
+SDL_Texture * BlackKeyTexture = IMG_LoadTexture(r1, (images_dir + "blackKeyPickUp.png").c_str());
+//Creating a Enemy 
+SDL_Rect Enemy;
+Enemy.x = 500;
+Enemy.y = 620;
+Enemy.w = 20;
+Enemy.h = 30;
+SDL_Texture * EnemyTexture = IMG_LoadTexture(r1, (images_dir + "placeholder.png").c_str());
 //Error Messages For Texture File Paths
 if (t1 == NULL)
 {
@@ -189,6 +261,14 @@ if(HealthPkUp1 == NULL)
 if(AmmoPkUp1 == NULL)
 {
 	cout <<"Error Loading Ammo Pick Up Texture :("<<endl;
+}
+if (HBarFront == NULL)
+{
+	cout << "Error Loading Health Bar :(" << endl;
+}
+if (HBarBack == NULL)
+{
+	cout << "Error Loading Health Bar Background :(" << endl;
 }
 if(window == NULL)
 {
@@ -227,6 +307,9 @@ while(inGame)
 				 case SDLK_d:
 				 PlayerVelX += Player_Vel;
 				 break;
+				 case SDLK_p:
+				 ManaBarFront.w -= 20;
+				 ammoCount -= 1;
 			  }
 		  }
 		  else if(event.type == SDL_KEYUP && event.key.repeat == 0)
@@ -255,107 +338,260 @@ while(inGame)
 	//Adjusting the screen Horizontally
 	Player.x += PlayerVelX;
 	Player.y += PlayerVelY;
-	/*if(Player.x > 1024 - (Player.w * 2))
-		{
-			Player.x = 1024 - (Player.w*2);
-			Background.x -= PlayerVelX;
+	//if(Player.x > 1024 - (Player.w * 2))
+	//	{
+	//		Player.x = 1024 - (Player.w*2);
+	//		Background.x -= PlayerVelX;
 
-			Wall.x -= PlayerVelX;
-			Wall2.x -= PlayerVelX;
-			Wall3.x -= PlayerVelX;
-			Wall4.x -= PlayerVelX;
-			Wall5.x -= PlayerVelX;
-			Wall6.x -= PlayerVelX;
-			Wall7.x -= PlayerVelX;
-			Wall8.x -= PlayerVelX;
-			Wall9.x -= PlayerVelX;
-			Wall10.x -= PlayerVelX;
-		}
-	if(Player.x < 0 + (Player.w * 2))
-			{
-				Player.x = 0 + (Player.w*2);
-				Background.x -= PlayerVelX;
+	//		Wall.x -= PlayerVelX;
+	//		Wall2.x -= PlayerVelX;
+	//		Wall3.x -= PlayerVelX;
+	//		Wall4.x -= PlayerVelX;
+	//		Wall5.x -= PlayerVelX;
+	//		Wall6.x -= PlayerVelX;
+	//		Wall7.x -= PlayerVelX;
+	//		Wall8.x -= PlayerVelX;
+	//		Wall9.x -= PlayerVelX;
+	//		Wall10.x -= PlayerVelX;
+	//		healthPickUp.x -= PlayerVelX;
+	//		ammoPickUp.x -= PlayerVelX;
+	//		Enemy.x -= PlayerVelX;
+	//	}
+	//if(Player.x < 0 + (Player.w * 2))
+	//		{
+	//			Player.x = 0 + (Player.w*2);
+	//			Background.x -= PlayerVelX;
 
-				Wall.x -= PlayerVelX;
-				Wall2.x -= PlayerVelX;
-				Wall3.x -= PlayerVelX;
-				Wall4.x -= PlayerVelX;
-				Wall5.x -= PlayerVelX;
-				Wall6.x -= PlayerVelX;
-				Wall7.x -= PlayerVelX;
-				Wall8.x -= PlayerVelX;
-				Wall9.x -= PlayerVelX;
-				Wall10.x -= PlayerVelX;
-			}
-	//Checking For collision with walls and the player Left and Right
-	if (SDL_HasIntersection(&Player, &Wall) || SDL_HasIntersection(&Player, &Wall2) ||
-		SDL_HasIntersection(&Player, &Wall3) || SDL_HasIntersection(&Player, &Wall4)||
-		SDL_HasIntersection(&Player, &Wall5) ||SDL_HasIntersection(&Player, &Wall6)||
-		SDL_HasIntersection(&Player, &Wall7)||SDL_HasIntersection(&Player, &Wall8)||
-		SDL_HasIntersection(&Player, &Wall9)||SDL_HasIntersection(&Player, &Wall10))
+	//			Wall.x -= PlayerVelX;
+	//			Wall2.x -= PlayerVelX;
+	//			Wall3.x -= PlayerVelX;
+	//			Wall4.x -= PlayerVelX;
+	//			Wall5.x -= PlayerVelX;
+	//			Wall6.x -= PlayerVelX;
+	//			Wall7.x -= PlayerVelX;
+	//			Wall8.x -= PlayerVelX;
+	//			Wall9.x -= PlayerVelX;
+	//			Wall10.x -= PlayerVelX;
+	//			healthPickUp.x -= PlayerVelX;
+	//			ammoPickUp.x -= PlayerVelX;
+	//			Enemy.x -= PlayerVelX;
+	//		}
+	////Checking For collision with walls and the player Left and Right
+	//if (SDL_HasIntersection(&Player, &Wall) || SDL_HasIntersection(&Player, &Wall2) ||
+	//	SDL_HasIntersection(&Player, &Wall3) || SDL_HasIntersection(&Player, &Wall4)||
+	//	SDL_HasIntersection(&Player, &Wall5) ||SDL_HasIntersection(&Player, &Wall6)||
+	//	SDL_HasIntersection(&Player, &Wall7)||SDL_HasIntersection(&Player, &Wall8)||
+	//	SDL_HasIntersection(&Player, &Wall9)||SDL_HasIntersection(&Player, &Wall10))
+	//{
+	//	Player.x -= PlayerVelX;
+	//}
+	//Enemy Collision
+	if (SDL_HasIntersection(&Player, &Enemy))
 	{
 		Player.x -= PlayerVelX;
+		HealthBarFront.w -= 10;
+		playerHealth -= 5;
 	}
+	//Key Collision
+	if (SDL_HasIntersection(&Player, &BlackKey))
+	{
+		Player.x -= PlayerVelX;
+		BlackKey.x = 0;
+		BlackKey.y = 0;
+		hasBlackKey = true;
+	}
+	if (SDL_HasIntersection(&Player, &PinkKey))
+	{
+		Player.x -= PlayerVelX;
+		PinkKey.x = 0;
+		PinkKey.y = 0;
+		hasPinkKey = true;
+	}
+	if (SDL_HasIntersection(&Player, &PurpleKey))
+	{
+		Player.x -= PlayerVelX;
+		PurpleKey.x = 0;
+		PurpleKey.y = 0;
+		hasPurpleKey = true;
+	}
+	//Health PickUp Collision
+		if (SDL_HasIntersection(&Player, &healthPickUp) && playerHealth < 100)
+		{
+			Player.x -= PlayerVelX;
+			HealthBarFront.w += 10;
+			playerHealth += 5;
+		}
+		if (playerHealth >= 100)
+		{
+			playerHealth = 100;
+			HealthBarFront.w = 200;
+		}
+		if (playerHealth <= 0)
+		{
+			playerHealth = 0;
+			HealthBarFront.w = 0;
+		}
+	//Mana PickUp Collision
+		if (SDL_HasIntersection(&Player, &ammoPickUp) && ammoCount < 10)
+		{
+			Player.x -= PlayerVelX;
+			ManaBarFront.w += 20;
+			ammoCount += 1;
+		}
+		if (ammoCount >= 10)
+		{
+			ammoCount = 10;
+			ManaBarFront.w = 200;
+		}
+		if (ammoCount <= 0)
+		{
+			ammoCount = 0;
+			ManaBarFront.w = 0;
+		}
+
 	//Adjusting the screen Vertically
-			if (Player.y < 0 + (Player.h * 2))
-			{
-				Player.y = 0 + (Player.h * 2);
-				Background.y -= PlayerVelY;
+			//if (Player.y < 0 + (Player.h * 2))
+			//{
+			//	Player.y = 0 + (Player.h * 2);
+			//	Background.y -= PlayerVelY;
 
-				Wall.y -= PlayerVelY;
-				Wall2.y -= PlayerVelY;
-				Wall3.y -= PlayerVelY;
-				Wall4.y -= PlayerVelY;
-				Wall5.y -= PlayerVelY;
-				Wall6.y -= PlayerVelY;
-				Wall7.y -= PlayerVelY;
-				Wall8.y -= PlayerVelY;
-				Wall9.y -= PlayerVelY;
-				Wall10.y -= PlayerVelY;
-			}
+			//	Wall.y -= PlayerVelY;
+			//	Wall2.y -= PlayerVelY;
+			//	Wall3.y -= PlayerVelY;
+			//	Wall4.y -= PlayerVelY;
+			//	Wall5.y -= PlayerVelY;
+			//	Wall6.y -= PlayerVelY;
+			//	Wall7.y -= PlayerVelY;
+			//	Wall8.y -= PlayerVelY;
+			//	Wall9.y -= PlayerVelY;
+			//	Wall10.y -= PlayerVelY;
+			//	healthPickUp.y -= PlayerVelY;
+			//	ammoPickUp.y -= PlayerVelY;
+			//	Enemy.y -= PlayerVelY;
+			//}
 
-			if(Player.y > 768 - (Player.h * 2))
-			{
-				Player.y = 768 - (Player.h * 2);
-				Background.y -= PlayerVelY;
+			//if(Player.y > 768 - (Player.h * 2))
+			//{
+			//	Player.y = 768 - (Player.h * 2);
+			//	Background.y -= PlayerVelY;
 
-				Wall.y -= PlayerVelY;
-				Wall2.y -= PlayerVelY;
-				Wall3.y -= PlayerVelY;
-				Wall4.y -= PlayerVelY;
-				Wall5.y -= PlayerVelY;
-				Wall6.y -= PlayerVelY;
-				Wall7.y -= PlayerVelY;
-				Wall8.y -= PlayerVelY;
-				Wall9.y -= PlayerVelY;
-				Wall10.y -= PlayerVelY;
-			}
+			//	Wall.y -= PlayerVelY;
+			//	Wall2.y -= PlayerVelY;
+			//	Wall3.y -= PlayerVelY;
+			//	Wall4.y -= PlayerVelY;
+			//	Wall5.y -= PlayerVelY;
+			//	Wall6.y -= PlayerVelY;
+			//	Wall7.y -= PlayerVelY;
+			//	Wall8.y -= PlayerVelY;
+			//	Wall9.y -= PlayerVelY;
+			//	Wall10.y -= PlayerVelY;
+			//	healthPickUp.y -= PlayerVelY;
+			//	ammoPickUp.y -= PlayerVelY;
+			//	Enemy.y -= PlayerVelY;
+			//}
 
 
-	//Checking For collision with walls Up and Down
-			if (SDL_HasIntersection(&Player, &Wall) || SDL_HasIntersection(&Player, &Wall2) ||
-				SDL_HasIntersection(&Player, &Wall3)|| SDL_HasIntersection(&Player, &Wall4)||
-				SDL_HasIntersection(&Player,&Wall5) || SDL_HasIntersection(&Player, &Wall6)||
-				SDL_HasIntersection(&Player, &Wall7)||SDL_HasIntersection(&Player, &Wall8)||
-				SDL_HasIntersection(&Player, &Wall9)||SDL_HasIntersection(&Player, &Wall10))
+			////Checking For collision with walls Up and Down
+			//if (SDL_HasIntersection(&Player, &Wall) || SDL_HasIntersection(&Player, &Wall2) ||
+			//	SDL_HasIntersection(&Player, &Wall3)|| SDL_HasIntersection(&Player, &Wall4)||
+			//	SDL_HasIntersection(&Player,&Wall5) || SDL_HasIntersection(&Player, &Wall6)||
+			//	SDL_HasIntersection(&Player, &Wall7)||SDL_HasIntersection(&Player, &Wall8)||
+			//	SDL_HasIntersection(&Player, &Wall9)||SDL_HasIntersection(&Player, &Wall10))
+			//{
+			//	Player.y -= PlayerVelY;
+			//}
+			//Enemy Collision
+			if (SDL_HasIntersection(&Player, &Enemy))
 			{
 				Player.y -= PlayerVelY;
-			}*/
+				HealthBarFront.w -= 10;
+				playerHealth -= 5;
+			}
+			//Key Collision
+			if (SDL_HasIntersection(&Player, &BlackKey) && hasBlackKey != true)
+			{
+				Player.y -= PlayerVelY;
+				BlackKey.x = 0;
+				BlackKey.y = 0;
+				hasBlackKey = true;
+			}
+			if (SDL_HasIntersection(&Player, &PinkKey) && hasPinkKey != true)
+			{
+				Player.y -= PlayerVelY;
+				PinkKey.x = 0;
+				PinkKey.y = 0;
+				hasPinkKey = true;
+			}
+			if (SDL_HasIntersection(&Player, &PurpleKey) && hasPurpleKey != true)
+			{
+				Player.y -= PlayerVelY;
+				PurpleKey.x = 0;
+				PurpleKey.y = 0;
+				hasPurpleKey = true;
+			}
+			//Health PickUp Collision
+			if (SDL_HasIntersection(&Player, &healthPickUp)) 
+			{
+				Player.y -= PlayerVelY;
+				HealthBarFront.w += 20;
+				playerHealth += 10;
+			}
+			//Mana PickUp Collision
+			if (SDL_HasIntersection(&Player, &ammoPickUp))
+			{
+				Player.y -= PlayerVelY;
+				ManaBarFront.w += 20;
+				ammoCount += 1;
+			}
 	//Update Player End//
 
 //SDL Drawing Process//
 // clear buffer and draw agents and walls.
 SDL_RenderClear(r1);
 //render a copy of the texture
-/*SDL_RenderCopy(r1, t1, NULL, &Background);*/
+//SDL_RenderCopy(r1, t1, NULL, &Background);
 //render what is presently in the buffer
 SDL_RenderCopy(r1,t2,NULL,&Player);
 //Rendering the health pickup
 SDL_RenderCopy(r1,HealthPkUp1,NULL,&healthPickUp);
 //Rendering the Ammo Pickup
 SDL_RenderCopy(r1,AmmoPkUp1,NULL,&ammoPickUp);
+//Rendering the Health Bar
+SDL_RenderCopy(r1, HBarBack, NULL, &HealthBarBack);
+SDL_RenderCopy(r1, HBarFront, NULL, &HealthBarFront);
+//Rendering the Ammo GUI
+SDL_RenderCopy(r1, MBarBack, NULL, &ManaBarBack);
+SDL_RenderCopy(r1, MBarFront, NULL, &ManaBarFront);
+//Rendering the enemy texture
+SDL_RenderCopy(r1, EnemyTexture, NULL, &Enemy);
+//Rendering the Keys in the level
+if (hasPinkKey != true)
+{
+	SDL_RenderCopy(r1, PinkKeyTexture, NULL, &PinkKey);
+}
+if (hasBlackKey != true)
+{
+	SDL_RenderCopy(r1, BlackKeyTexture, NULL, &BlackKey);
+}
+if (hasPurpleKey != true)
+{
+	SDL_RenderCopy(r1, PurpleKeyTexture, NULL, &PurpleKey);
+}
+//Rendering the Keys for the GUI
+if (hasPinkKey == true)
+{
+	SDL_RenderCopy(r1, PinkKeyGUITexture, NULL, &PinkKeyGUI);
+}
+if (hasBlackKey == true)
+{
+	SDL_RenderCopy(r1, BlackKeyGUITexture, NULL, &BlackKeyGUI);
+}
+if (hasPurpleKey == true)
+{
+	SDL_RenderCopy(r1, PurpleKeyGUITexture, NULL, &PurpleKeyGUI);
+}
 //Wall Texture Code
-/*SDL_RenderCopy(r1,w1,NULL,&Wall);
+SDL_RenderCopy(r1,w1,NULL,&Wall);
 
 SDL_RenderCopy(r1,w2,NULL,&Wall2);
 
@@ -373,7 +609,7 @@ SDL_RenderCopy(r1, w8, NULL, &Wall8);
 
 SDL_RenderCopy(r1, w9, NULL, &Wall9);
 
-SDL_RenderCopy(r1, w10, NULL, &Wall10);*/
+SDL_RenderCopy(r1, w10, NULL, &Wall10);
 
 SDL_RenderPresent(r1);
 //SDL Drawing Process End//
